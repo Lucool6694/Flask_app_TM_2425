e2bfd92a085aef59b814d376f6ebed6361502eba
Lucool6694 <180522474+Lucool6694@users.noreply.github.com>
2025-02-27 16:54:30 +0100

    ajout de la page de vision des rendez-vous pour le cabinet

--- app/views/rendez_vous.py
+++ app/views/rendez_vous.py
@@ -3,6 +3,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from app.db.db import get_db, close_db
 import os
 from sqlite3 import connect
+from datetime import datetime
 
 
 
@@ -112,4 +113,49 @@ def reserver():
         return render_template('rdv/rendez-vous_motif.html', date=selected_date, time=selected_time)
 
     
-             
+@rdv_bp.route('/planing', methods=['GET', 'POST'])
+def planing():
+    selected_date = request.args.get('date')
+    all_time_slots = [
+        "08:00", "10:00", "12:00", "14:00", "16:00", "18:00"
+    ]
+
+
+    if not selected_date:
+        selected_date = datetime.today().strftime('%Y-%m-%d')
+    
+    
+    db = get_db()
+    cur = db.cursor()
+
+    
+    cur.execute('''
+        SELECT rv.heure, p.nom, rv.motif 
+        FROM "rendez-vous" rv
+        JOIN "personnes" p ON rv.id_personne = p.id_personne
+        WHERE rv.date = ?
+    ''', (selected_date,))
+    
+    reservations = cur.fetchall()
+    occupied_slots = [row[0] for row in cur.fetchall()]
+    db.commit()
+    close_db()
+    
+    
+    if not reservations:
+        return render_template('rdv/rendez-vous_planing.html', date=selected_date, time_slots={})
+    
+
+    
+
+    reservation_details = {row["heure"]: {"Nom": row["nom"], "Motif": row["motif"]} for row in reservations}
+
+# Create the final time_slots dictionary
+    time_slots = {
+        slot: reservation_details[slot] if slot in reservation_details else "Disponible"
+        for slot in all_time_slots
+    }
+
+    return render_template('rdv/rendez-vous_planing.html', date=selected_date, time_slots=time_slots)
+
+    
